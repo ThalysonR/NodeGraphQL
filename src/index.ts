feff6aee3bbd659e7f5b91ab78535475e3eabefd
Thalyson <ThalysonR.castro@hotmail.com>
2019-04-18 15:18:02 -0400

    Ajuste onError

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 
 import app from './app';
 import db from './models';
-import { normalizePort, onError } from './utils/utils';
+import { normalizePort } from './utils/utils';
+import { Server } from 'http';
 
 const port = normalizePort(process.env.port || 3000);
 const host = process.env.host || '127.0.0.1';
@@ -13,3 +14,24 @@ db.sequelize.sync()
             console.log(`🚀Server ready at ${url}`);
         }).catch(onError);
     });
+
+const onError = (server: Server) => {
+    return (error: NodeJS.ErrnoException): void => {
+        // @ts-ignore
+        const port: number | string = server.address().port;
+        if (error.syscall !== 'listen') { throw error; }
+        const bind = (typeof port === 'string') ? `pipe ${port}` : `port ${port}`;
+        switch (error.code) {
+            case 'EACCES':
+                console.error(`${bind} requires elevated privileges`);
+                process.exit(1);
+                break;
+            case 'EADDRINUSE':
+                console.error(`${bind} is already in use`);
+                process.exit(1);
+                break;
+            default:
+                throw error;
+        }
+    }
+};
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,28 +1,5 @@
-import { Server } from "http";
-
 export const normalizePort = (val: number | string): number => {
     return (typeof val === 'string') ? parseInt(val) : val;
-};
-
-export const onError = (server: Server) => {
-    return (error: NodeJS.ErrnoException): void => {
-        // @ts-ignore
-        const port: number | string = server.address().port;
-        if (error.syscall !== 'listen') { throw error; }
-        const bind = (typeof port === 'string') ? `pipe ${port}` : `port ${port}`;
-        switch(error.code) {
-            case 'EACCES':
-                console.error(`${bind} requires elevated privileges`);
-                process.exit(1);
-                break;
-            case 'EADDRINUSE':
-                console.error(`${bind} is already in use`);
-                process.exit(1);
-                break;
-            default:
-                throw error;
-        }
-    }
 };
 
 export const handleError = (error: Error) => {
