9f55136430093773c1654c75c1b76b127d57cbf9
deyveson <deyvesonn@outlook.com>
2019-04-25 15:34:09 -0400

    test application

--- src/graphql/resources/catalogo/aplicacoes.datasources.ts
+++ src/graphql/resources/catalogo/aplicacoes.datasources.ts
@@ -1,17 +1,17 @@
-import { RESTDataSource } from 'apollo-datasource-rest'
+import { RESTDataSource } from 'apollo-datasource-rest';
 
 class AplicacoesApi extends RESTDataSource {
   constructor() {
-    super()
+    super();
     // @ts-ignore
-    this.initialize({ context: {} })
-    this.baseURL = 'http://192.168.151.89:8000/ws-catalogo/api/'
+    this.initialize({ context: {} });
+    this.baseURL = 'http://192.168.151.89:8000/ws-catalogo/api/';
   }
 
   public async searchAplicacoes(buscaAplicacoes) {
-    const aplicacoes = await this.get('produto/aplicacoes', buscaAplicacoes)
-    return aplicacoes
+    const aplicacoes = await this.get('produto/aplicacoes', buscaAplicacoes);
+    return aplicacoes;
   }
 }
 
-export default AplicacoesApi
+export default AplicacoesApi;
