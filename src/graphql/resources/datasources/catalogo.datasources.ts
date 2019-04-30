841038909723929222e8289d9db358fd4940bb49
deyveson <deyvesonn@outlook.com>
2019-04-30 11:04:39 -0400

    catalogo

--- src/graphql/resources/datasources/catalogo.datasources.ts
+++ src/graphql/resources/datasources/catalogo.datasources.ts
@@ -8,52 +8,9 @@ class CatalogoAPI extends RESTDataSource {
     this.baseURL = 'http://192.168.151.89:8000/ws-catalogo/api/';
   }
 
-  public async searchProduto(text) {
-    const nomeProduto = text.args.text;
-
-    const response = await this.get(
-      'produto?page=0&count=30&order=DESC&sort=frequencia&nomeProduto=' + nomeProduto,
-    );
-
-    return Array.isArray(response.produtos.content)
-      ? response.produtos.content.map(produto => this.produtoReducer(produto))
-      : [];
-  }
-
-  public produtoReducer(produto) {
-    return {
-      id: produto.id || 0,
-      idEmpresa: produto.idEmpresa,
-      idFornecedor: produto.idFornecedor,
-      nomeFornecedor: produto.nomeFornecedor,
-      codigoProduto: produto.codigoProduto,
-      codigoOriginalProduto: produto.codigoOriginalProduto,
-      nomeProduto: produto.nomeProduto,
-      frequencia: produto.frequencia,
-      codProduto: produto.codProduto,
-      score: produto.score,
-      tags: produto.tags,
-      carType: produto.carType,
-      carId: produto.carId,
-      articleNo: produto.articleNo,
-      manuId: produto.manuId,
-      models: produto.models,
-      anos: produto.anos,
-      modeloCarro: produto.modeloCarro,
-      fabricante: produto.fabricante,
-      eixo: produto.eixo,
-      posicao: produto.posicao,
-      lado: produto.lado,
-      motor: produto.motor,
-      combustivel: produto.combustivel,
-      aplicacao: produto.aplicacao,
-      montadoras: produto.montadoras,
-      prefixo: produto.prefixo,
-      aro: produto.aro,
-      perfil: produto.perfil,
-      viscosidade: produto.viscosidade,
-      amperagem: produto.amperagem,
-    };
+  public async searchProduto(pesqProduto) {
+    const response = await this.get('produto', pesqProduto);
+    return response;
   }
 
   public async searchAplicacoes(buscaAplicacoes) {
--- src/graphql/resources/schemas/catalogo.schema.ts
+++ src/graphql/resources/schemas/catalogo.schema.ts
@@ -1,60 +1,108 @@
 const produtoTypes = `
-    type Produto {
-        id:  ID!
-        idEmpresa: Int!
-        idFornecedor: Int!
-        nomeFornecedor: String!
-        codigoProduto: String!
-        codigoOriginalProduto: Int
-        nomeProduto: String!
-        frequencia: Int!
-        codProduto: String!
-        score: Int
-        tags: String
-        carType: String
-        carId: Int
-        articleNo: Int
-        manuId: Int
-        models: [Models!]!
-        modeloCarro: String
-        fabricante: String
-        anos: [String]
-        eixo: [String]
-        posicao: [String]
-        lado: [String]
-        motor: [String]
-        combustivel: [String]
-        aplicacao: [String]
-        montadoras: [String]
-        prefixo: String
-        aro: String
-        perfil: String
-        viscosidade: String
-        amperagem: String
-    }
+type Data{
+    produtos: Produtos!
+    tags: [String]!
+}
 
-    type Models{
-        nomeCarro: String!
-        modelTipos: [ModelTipos]
-    }
+type Produtos{
+    content: [Content]!
+    pageable: Pageable
+    facets: [String]
+    aggregations: Aggregations
+    scrollId: String
+    totalPages: Int
+    totalElements: Int
+    sort: Sort
+    first: Boolean
+    last: Boolean
+    numberOfElements: Int
+    size: Int
+    number: Int
+}
 
-    type ModelTipos{
-        tipoNome: String
-        geracao: String
-        motor: String
-        anos: [String]
-        nomeMotor: String
-        modeloTransmissao: String
-        eixoMotriz: String
-    }
+type Content {
+    id:  ID!
+    idEmpresa: Int!
+    idFornecedor: Int!
+    nomeFornecedor: String!
+    codigoProduto: String!
+    codigoOriginalProduto: Int
+    nomeProduto: String!
+    frequencia: Int!
+    codProduto: String!
+    score: Int
+    tags: String
+    carType: String
+    carId: Int
+    articleNo: Int
+    manuId: Int
+    models: [Models!]!
+    modeloCarro: String
+    fabricante: String
+    anos: [String]
+    eixo: [String]
+    posicao: [String]
+    lado: [String]
+    motor: [String]
+    combustivel: [String]
+    aplicacao: [String]
+    montadoras: [String]
+    prefixo: String
+    aro: String
+    perfil: String
+    viscosidade: String
+    amperagem: String
+}
 
-    type Tags{
-        tags: [String]
-    }
+type Models{
+    nomeCarro: String!
+    modelTipos: [ModelTipos]
+}
+
+type ModelTipos{
+    tipoNome: String
+    geracao: String
+    motor: String
+    anos: [String]
+    nomeMotor: String
+    modeloTransmissao: String
+    eixoMotriz: String
+}
+
+type Pageable{
+    sort: Sort
+    pageSize: Int
+    pageNumber: Int
+    offset: Int
+    unpaged: Boolean
+    paged: Boolean
+}
+
+type Sort{
+    unsorted: Boolean
+    sorted: Boolean
+}
+
+type Aggregations{
+    fragment: Boolean
+    asMap: AsMap
+}
+
+type AsMap{
+    asMap: String
+}
+
+input PesqProduto{
+    page: Int!
+    count: Int!
+    order: String!
+    sort: String!
+    nomeProduto: String!
+}
 `;
 
 const produtoQueries = `
-    getproduto(text: String!): [Produto!]!
+    getproduto(pesqProduto: PesqProduto!): Data!
 `;
 
 const produtoMutations = `
