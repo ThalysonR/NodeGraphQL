d424e80cd99074d12a59abb7409a13a80b5eb0f3
deyveson <deyvesonn@outlook.com>
2019-05-02 15:32:56 -0400

    implementations PessoaFisica and PessoaJuridica

--- src/graphql/resources/resolvers/pessoa.resolvers.ts
+++ src/graphql/resources/resolvers/pessoa.resolvers.ts
@@ -1,4 +1,4 @@
-import { handleError, gqlCompose } from '../../../utils/utils';
+import { gqlCompose } from '../../../utils/utils';
 import { authResolvers } from '../../composable/auth.resolver';
 import { ResolverContext } from '../../../interfaces/ResolverContextInterface';
 
@@ -6,9 +6,24 @@ export const pessoaResolvers = {
   Query: {
     getPessoa: gqlCompose(...authResolvers)(
       async (parent, args, { dataSources }: ResolverContext) => {
-        const cpf = Object.assign({ args });
-        return dataSources.pessoaApi.searchPessoa(cpf).catch(handleError);
+        const endpoint =
+          args.text.length > 11
+            ? dataSources.pessoaApi.searchPessoaJuridica
+            : dataSources.pessoaApi.searchPessoa;
+        const pessoa = await endpoint.call(dataSources.pessoaApi, args.text);
+        return pessoa;
       },
     ),
+  },
+  PessoaCadastro: {
+    __resolveType(obj, context, info) {
+      if (obj.cpf) {
+        return 'PessoaFisica';
+      } else if (obj.cnpj) {
+        return 'PessoaJuridica';
+      } else {
+        return null;
+      }
+    },
   },
 };
